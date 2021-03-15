af4d7449a57a0d6e37dc63ad18fa183cfd92d693
Pavel <p.6a@tut.by>
2021-03-15 21:03:03 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    
+  return array && array.length ? Math.min(...array) : 0;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    return array && array.length ? Math.max(...array) : 0;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let result = 0;
+    if (array && array.length) {
+        for (let i = 0; i < array.length; i++) {
+            result = result + array[i];
+        }
+        result = result / array.length;
+    }
+  return result;
+};
